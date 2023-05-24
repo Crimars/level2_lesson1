8a576ea8b871c4b46f42414c7a2ca7134e12a8fc
Crimars_work <isogd.kr@gmail.com>
2023-05-24 09:58:24 +0300

    проверка 2

--- src/lesson2/Main.java
+++ src/lesson2/Main.java
@@ -25,7 +25,7 @@ public class Main {
             }
         }
         catch (MyArrayDataException e) {
-            System.out.println("Неверные данные");
+            System.out.println("Неверные данные ");
             System.out.println("Ошибка в ячейке");
         }
 
